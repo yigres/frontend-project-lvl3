df78e08c5b8c0bc784baa894c38f593291f81da6
Sergey Kirillov <s_kirillov@rambler.ru>
2021-01-12 14:09:34 +0300

    added Webpack, Bootstrap, template.html

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
+build:
+	npx webpack
+	
 install:
 	npm ci
 	
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  // entry: './src/index.js',
+  // output: {
+  //   filename: 'bundle.js',
+  //   path: path.resolve(__dirname, 'dist'),
+  // },
+  // mode: process.env.NODE_ENV || 'development',
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.js$/,
+  //       exclude: /node_modules/,
+  //       use: {
+  //         loader: 'babel-loader',
+  //       },
+  //     },
+  //   ],
+  // },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'template.html',
+    }),
+  ],
+};
