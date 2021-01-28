df56b19b6c0e11c47c48d012e1aaffa8a5244572
Sergey Kirillov <s_kirillov@rambler.ru>
2021-01-28 13:53:44 +0300

    added i18next

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 // import 'bootstrap';
+import i18n from 'i18next';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import * as yup from 'yup';
 // import _ from 'lodash';
@@ -14,14 +15,11 @@ const state = {
       status: null,
     },
   },
-  rss: {
-    state: null, // Rss has been loaded / Must be valid url / Rss already exists
-  },
   feeds: [],
   posts: [],
 };
 
-const isFeedExist = (value) => {
+const FeedExists = (value) => {
   let result = false;
   state.feeds.forEach(({ url }) => {
     if (url === value) {
@@ -35,8 +33,10 @@ const isFeedExist = (value) => {
 const watchedState = view(state);
 
 const parser = (data, url, oldFeeds, oldPosts) => {
-  const feeds = JSON.parse(JSON.stringify(oldFeeds));
-  const posts = JSON.parse(JSON.stringify(oldPosts));
+  const clonedArray = (arr) => JSON.parse(JSON.stringify(arr));
+
+  const feeds = clonedArray(oldFeeds);
+  const posts = clonedArray(oldPosts);
   const dom = new DOMParser();
   const doc = dom.parseFromString(data.contents, 'text/xml');
   const feedName = doc.querySelector('title').textContent;
@@ -74,43 +74,74 @@ const schema = yup.object().shape({
 
 const form = document.querySelector('form');
 
-form.addEventListener('submit', (e) => {
-  const url = form.querySelector('input').value;
-  e.preventDefault();
-  if (isFeedExist(url)) {
-    watchedState.form.state.valid = false;
-    watchedState.form.state.status = 'Rss already exists';
-  }
-  if (!isFeedExist(url)) {
-    schema
-      .isValid({
-        website: url,
-      })
-      .then((valid) => {
-        state.form.state.url = url;
-        if (valid === false) {
-          watchedState.form.state.valid = false;
-          watchedState.form.state.status = 'Must be valid url';
-        }
-        if (valid === true) {
-          form.querySelector('input').value = '';
-          watchedState.form.state.valid = true;
-          fetch(`https://hexlet-allorigins.herokuapp.com/get?url=${encodeURIComponent(url)}`)
-            .then((response) => {
-              if (response.ok) {
-                watchedState.form.state.status = 'Rss has been loaded';
-                return response.json();
-              }
-              throw new Error('Network response was not ok.');
-            })
-            .then((data) => {
-              const { feeds, posts } = parser(data, url, state.feeds, state.posts);
-              state.posts = posts;
-              watchedState.feeds = feeds;
-            });
-        }
-      });
-  }
-});
+i18n.init({
+  lng: 'en',
+  debug: true,
+  resources: {
+    en: {
+      translation: {
+        form: {
+          status: {
+            loaded: 'Rss has been loaded',
+            invalid: 'Must be valid url',
+            duplicated: 'Rss already exists',
+          },
+        },
+      },
+    },
+  },
+})
+  .then(() => {
+    form.addEventListener('submit', (e) => {
+      const url = form.querySelector('input').value;
+      e.preventDefault();
+      if (FeedExists(url)) {
+        watchedState.form.state.valid = false;
+        watchedState.form.state.status = i18n.t('form.status.duplicated');
+      }
+      if (!FeedExists(url)) {
+        schema
+          .isValid({
+            website: url,
+          })
+          .then((valid) => {
+            state.form.state.url = url;
+            if (valid === false) {
+              watchedState.form.state.valid = false;
+              watchedState.form.state.status = i18n.t('form.status.invalid');
+            }
+            if (valid === true) {
+              form.querySelector('input').value = '';
+              watchedState.form.state.valid = true;
+              fetch(`https://hexlet-allorigins.herokuapp.com/get?url=${encodeURIComponent(url)}`)
+                .then((response) => {
+                  if (response.ok) {
+                    watchedState.form.state.status = i18n.t('form.status.loaded');
+                    return response.json();
+                  }
+                  throw new Error('Network response was not ok.');
+                })
+                .then((data) => {
+                  const { feeds, posts } = parser(data, url, state.feeds, state.posts);
+                  state.posts = posts;
+                  watchedState.feeds = feeds;
+                })
+                .catch((er) => {
+                  console.error('Something went wrong with response');
+                  console.error(er);
+                });
+            }
+          })
+          .catch((error) => {
+            console.error('Something went wrong url validation');
+            console.error(error);
+          });
+      }
+    });
+  })
+  .catch((e) => {
+    console.error('Something went wrong with i18next initialization');
+    console.error(e);
+  });
 
 export default state;
