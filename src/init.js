dc780075691fca889ed1dcfbd837bbce22630481
Sergey Kirillov <s_kirillov@rambler.ru>
2021-02-05 14:58:18 +0300

    added entry point to package.json

---
+++ src/init.js
@@ -0,0 +1,213 @@
+import i18n from 'i18next';
+import * as yup from 'yup';
+
+import view from './view.js';
+
+const state = {
+  form: {
+    state: {
+      valid: null,
+      url: null,
+      status: null,
+    },
+  },
+  feeds: [],
+  posts: [],
+};
+
+const FeedExists = (value) => {
+  let result = false;
+  state.feeds.forEach(({ url }) => {
+    if (url === value) {
+      result = true;
+    }
+  });
+
+  return result;
+};
+
+// const watchedState = view(state);
+// ***************
+const parser = (data, url, oldFeeds, oldPosts) => {
+  const clonedArray = (arr) => JSON.parse(JSON.stringify(arr));
+
+  const feeds = clonedArray(oldFeeds);
+  const posts = clonedArray(oldPosts);
+  const dom = new DOMParser();
+  const doc = dom.parseFromString(data.contents, 'text/xml');
+  const feedName = doc.querySelector('title').textContent;
+  const feedDescription = doc.querySelector('description').textContent;
+  const feedId = oldFeeds.length + 1;
+  const items = doc.querySelectorAll('item');
+
+  for (let i = 1; i <= items.length; i += 1) {
+  // items.forEach((item) => {
+    const item = items[items.length - i];
+    const name = item.querySelector('title').textContent;
+    const description = item.querySelector('description').textContent;
+    const link = item.querySelector('link').textContent;
+    posts.push({
+      postId: oldPosts.length + i,
+      feedId,
+      name,
+      description,
+      link,
+      unread: true,
+    });
+  }
+
+  feeds.push({
+    feedId,
+    name: feedName,
+    description: feedDescription,
+    url,
+  });
+
+  return { feeds, posts };
+};
+// ***************
+const checkFeeds = (watchedState) => {
+  state.feeds.forEach((feed) => {
+    fetch(`https://hexlet-allorigins.herokuapp.com/get?disableCache=true&url=${encodeURIComponent(feed.url)}`)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error('Network response was not ok.');
+      })
+      .then((data) => {
+        const { posts } = parser(data, feed.url, [], []);
+        const newPosts = posts.map((newPost) => ({
+          name: newPost.name,
+          description: newPost.description,
+          link: newPost.link,
+        }));
+        const oldPostUrls = state.posts
+          .filter((postValue) => postValue.feedId === feed.feedId)
+          .map((postValue) => postValue.link);
+        newPosts.forEach((newPost) => {
+          if (oldPostUrls.indexOf(newPost.link) === (-1)) {
+            const postId = state.posts.length + 1;
+            watchedState.posts.push({
+              postId,
+              feedId: feed.feedId,
+              name: newPost.name,
+              description: newPost.description,
+              link: newPost.link,
+              unread: true,
+            });
+          }
+        });
+      })
+      .catch((er) => {
+        console.error('Something went wrong with response');
+        console.error(er);
+      });
+  });
+};
+
+export default () => {
+  const watchedState = view(state);
+
+  const schema = yup.object().shape({
+    website: yup.string().url(),
+  });
+
+  const form = document.querySelector('form');
+
+  i18n.init({
+    lng: 'en',
+    debug: true,
+    resources: {
+      en: {
+        translation: {
+          form: {
+            status: {
+              loaded: 'Rss has been loaded',
+              invalid: 'Must be valid url',
+              duplicated: 'Rss already exists',
+            },
+          },
+        },
+      },
+    },
+  })
+    .then(() => {
+      form.addEventListener('submit', (e) => {
+        const url = form.querySelector('input').value;
+        e.preventDefault();
+        if (FeedExists(url)) {
+          watchedState.form.state.valid = false;
+          watchedState.form.state.status = i18n.t('form.status.duplicated');
+        }
+        if (!FeedExists(url)) {
+          schema
+            .isValid({
+              website: url,
+            })
+            .then((valid) => {
+              state.form.state.url = url;
+              if (valid === false) {
+                watchedState.form.state.valid = false;
+                watchedState.form.state.status = i18n.t('form.status.invalid');
+              }
+              if (valid === true) {
+                form.querySelector('input').value = '';
+                watchedState.form.state.valid = true;
+                fetch(`https://hexlet-allorigins.herokuapp.com/get?disableCache=true&url=${encodeURIComponent(url)}`)
+                  .then((response) => {
+                    if (response.ok) {
+                      watchedState.form.state.status = i18n.t('form.status.loaded');
+                      return response.json();
+                    }
+                    throw new Error('Network response was not ok.');
+                  })
+                  .then((data) => {
+                    const { feeds, posts } = parser(data, url, state.feeds, state.posts);
+                    state.posts = posts;
+                    watchedState.feeds = feeds;
+                    // console.log(state.posts);
+                  })
+                  .catch((er) => {
+                    console.error('Something went wrong with response');
+                    console.error(er);
+                  });
+              }
+            })
+            .catch((error) => {
+              console.error('Something went wrong url validation');
+              console.error(error);
+            });
+        }
+      });
+
+      const modalEl = document.getElementById('previewModal');
+
+      modalEl.addEventListener('show.bs.modal', (event) => {
+        const { id } = event.relatedTarget.dataset;
+        const {
+          name,
+          description,
+          link,
+        } = state.posts[id - 1];
+        const modal = event.target;
+        modal.querySelector('.modal-title').textContent = name;
+        modal.querySelector('.modal-body').textContent = description;
+        modal.querySelector('div a').href = link;
+        console.log(id - 1, state.posts[id - 1].unread);
+        watchedState.posts[id - 1].unread = false;
+        console.log(id - 1, state.posts[id - 1].unread);
+      });
+
+      setTimeout(function tick() {
+        setTimeout(tick, 5000);
+        checkFeeds(watchedState);
+      }, 5000);
+    })
+    .catch((e) => {
+      console.error('Something went wrong with i18next initialization');
+      console.error(e);
+    });
+};
+
+// export default state;
